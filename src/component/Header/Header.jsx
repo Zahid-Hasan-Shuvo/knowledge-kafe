bb9e09c6d69b81e445dcf7d6f17ed71d8b1e1981
Zahid-Hasan-Shuvo <shuvo.cse6.bu@gmail.com>
2023-04-15 11:30:25 +0600

    header

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <nav className='header'>
+ 
+   <h3>Knowledge Cafe</h3> 
+   
+         <div>
+         <a href="/shop">shop</a>
+           <a href="/order">order</a>
+           <a href="/login">login</a>
+           <a href="/logout">logout</a>
+         </div>
+         
+       </nav>
+  );
+};
+
+export default Header;
